a2e164a30a7256e3d9d626ebd7d684b18b33c38e
Kevin Barreto <barretokevin@hotmail.com>
2024-12-26 07:23:49 -0600

    add loader and not found pages

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,7 @@
-import { Button } from "@/components/ui/button";
-
-export default function Home() {
+export default async function Home() {
   return (
     <div>
       <h1>Pro Store</h1>
-      <Button>Hello World</Button>
     </div>
   );
 }
