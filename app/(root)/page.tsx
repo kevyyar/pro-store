149c8592c824eef3766d4cead45137e1f890d24a
Kevin Barreto <barretokevin@hotmail.com>
2024-12-26 15:11:56 -0600

    add product list with sample data

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,15 @@
+import ProductList from "@/components/shared/product/product-list";
+import sampleData from "@/db/sample-data";
+
 export default async function Home() {
   return (
     <div>
       <h1>Pro Store</h1>
+      <ProductList
+        data={sampleData.products}
+        title="Featured Products"
+        limit={4}
+      />
     </div>
   );
 }
