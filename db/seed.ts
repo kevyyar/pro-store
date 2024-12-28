1c333d580b9d98526c4890da3451bf2c4af55079
Kevin Barreto <barretokevin@hotmail.com>
2024-12-28 15:14:39 -0600

    add sample data with users

--- db/seed.ts
+++ db/seed.ts
@@ -5,9 +5,26 @@ async function seed() {
   // delete all products
   await prisma.product.deleteMany();
 
+  // delete all accounts
+  await prisma.account.deleteMany();
+
+  // delete all sessions
+  await prisma.session.deleteMany();
+
+  // delete verification tokens
+  await prisma.verificationToken.deleteMany();
+
+  // delete all users
+  await prisma.user.deleteMany();
+
   // create new products
   await prisma.product.createMany({
     data: sampleData.products,
+  });
+
+  // create new users
+  await prisma.user.createMany({
+    data: sampleData.users,
   });
 
   console.log("Seed completed");
